22c98fa8fb748132496a79a62c75e5759139affb
kotikivborsche <dimabobrov1998@yandex.ru>
2022-10-16 10:08:02 +0500

    Add bad burger

--- script/script.js
+++ script/script.js
@@ -7,9 +7,8 @@ window.addEventListener('scroll', function() {
    else {
         elem.classList.remove('active');
    }
-});
+})
 ////
-
 //// faq-questions
 let elems = document.querySelectorAll('.faq-question');
 elems.forEach(elem => elem.addEventListener('click', function () {
@@ -18,37 +17,108 @@ elems.forEach(elem => elem.addEventListener('click', function () {
     }
     else {
         elem.classList.add('active');
-    }
-    
+    }  
 }))
 ////
-
+//// Открытие модальных форм
 const module_open__buttons = document.querySelectorAll('.js-modal-open');
 module_open__buttons.forEach(btn => btn.addEventListener('click',function(){
     const attribute = btn.getAttribute('data-open');
     const modal__form = document.querySelector(attribute);
-    modal__form.classList.add('_active');
+    modal__form.classList.add('active');
 }))
-
+////
+//// закрытие на крестик модальных форм
 const module_close__btns = document.querySelectorAll('.modal__close');
-
 module_close__btns.forEach(btn => btn.addEventListener('click',function(){
     const parent = btn.closest('.modal');
-    parent.classList.remove('_active');
-   
+    parent.classList.remove('active');
 }))
-
-
+////
+//// закрытие модальной формы при клике на внешнюю область 
 const modals = document.querySelectorAll('.modal');
 modals.forEach(modal => modal.addEventListener('click', function(event){
-
     if(event.target.classList.contains('modal')){
-        modal.classList.remove('_active');
+        modal.classList.remove('active');
     }
-
-   
-
-
-
-
 }))
+////
+//// свайпер
+const swiper1 = new Swiper('.swiper1', {
+    // Optional parameters
+    direction: 'horizontal',
+    slidesPerView:1.2,
+    //spaceBetween: 50,
+    loop: false,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    breakpoints: {
+        // when window width is >= 320px
+        376: {
+          slidesPerView: 4
+          
+        },
+        // when window width is >= 480px
+        // 480: {
+        //   slidesPerView: 3,
+        //   spaceBetween: 30
+        // },
+        // // when window width is >= 640px
+        // 640: {
+        //   slidesPerView: 4,
+        //   spaceBetween: 40
+        // }
+      }
+    
+  });
+  const swiper2 = new Swiper('.swiper2', {
+    // Optional parameters
+    direction: 'horizontal',
+    slidesPerView:1,
+    //spaceBetween: 50,
+    loop: false,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    breakpoints: {
+        // when window width is >= 320px
+        376: {
+          slidesPerView: 2
+          
+        },
+        // when window width is >= 480px
+        // 480: {
+        //   slidesPerView: 3,
+        //   spaceBetween: 30
+        // },
+        // // when window width is >= 640px
+        // 640: {
+        //   slidesPerView: 4,
+        //   spaceBetween: 40
+        // }
+      }
+    
+  });
+
+
+
+
+  ////
+  //// выезжающий бургер
+let burger__btn = document.querySelector('.burger__button-svg');
+burger__btn.addEventListener('click',function(){
+    let elem = document.querySelector('.menu-items');
+    let elem_body = document.querySelector('body');
+    let elem_burg = document.querySelector('.burger');
+    elem.classList.toggle('active');
+    elem_body.classList.toggle('noscroll');
+    elem_burg.classList.toggle('active');
+})
+
