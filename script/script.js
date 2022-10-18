2a60025a2a1e3e032287e46c39daf3e121b29815
kotikivborsche <dimabobrov1998@yandex.ru>
2022-10-19 00:04:35 +0500

    add adaptive layout

--- script/script.js
+++ script/script.js
@@ -43,6 +43,46 @@ modals.forEach(modal => modal.addEventListener('click', function(event){
     }
 }))
 ////
+//// закрытие бургера при выборе рациона
+const burger__btn__ration = document.querySelector('.big-boop__button');
+burger__btn__ration.addEventListener('click',function(){
+  let elem = document.querySelector('.menu-items');
+  let elem_body = document.querySelector('body');
+  let elem_burg = document.querySelector('.burger');
+  elem.classList.toggle('active');
+  elem_body.classList.toggle('noscroll');
+  elem_burg.classList.toggle('active');
+})
+////
+//// закрытие бургера при клике на лого
+const burger__btn__logo = document.querySelector('.logo');
+burger__btn__logo.addEventListener('click',function(){
+  let elem_burg = document.querySelector('.burger');
+  if (elem_burg.classList.contains('active')) {
+    let elem = document.querySelector('.menu-items');
+    let elem_body = document.querySelector('body');
+    
+    elem.classList.toggle('active');
+    elem_body.classList.toggle('noscroll');
+    elem_burg.classList.toggle('active');
+  }
+  
+})
+////
+//// закрытие бургера при выборе пункта меню
+const burger__btns = document.querySelectorAll('.menu-item');
+burger__btns.forEach(btn => btn.addEventListener('click',function(){
+  let elem = document.querySelector('.menu-items');
+  let elem_body = document.querySelector('body');
+  let elem_burg = document.querySelector('.burger');
+  elem.classList.toggle('active');
+  elem_body.classList.toggle('noscroll');
+  elem_burg.classList.toggle('active');
+}))
+////
+
+
+
 //// свайпер
 const swiper1 = new Swiper('.swiper1', {
     // Optional parameters
@@ -121,4 +161,5 @@ burger__btn.addEventListener('click',function(){
     elem_body.classList.toggle('noscroll');
     elem_burg.classList.toggle('active');
 })
+////
 
